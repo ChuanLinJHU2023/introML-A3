c49531d80360076b2641f9c0c2dd3039505f60ef
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2024-03-12 20:45:58 +0800

    3.12.20.45

--- DataPredictors.py
+++ DataPredictors.py
@@ -17,13 +17,13 @@ class Layer:
 
 
 class Linear(Layer):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, W=None, b=None):
         self.input_size=input_size
         self.output_size=output_size
         self.input=None
         self.output=None
-        self.W=np.random.randn(output_size,input_size)
-        self.b=np.random.randn(output_size,1)
+        self.W=np.random.randn(output_size,input_size) if W is None else W
+        self.b=np.random.randn(output_size,1) if b is None else b
         self.W_grad=None
         self.b_grad=None
 
@@ -40,6 +40,8 @@ class Linear(Layer):
         input_gradient=output_gradient@self.W
         self.W_grad=output_gradient@self.input
         self.b_grad=output_gradient
+        assert self.W_grad.shape==self.W.shape
+        assert self.b_grad.shape==self.b.shape
         return input_gradient
 
     def param(self, parameter_name):
@@ -57,3 +59,23 @@ class Linear(Layer):
         if parameter_name=="b":
             return self.b_grad
         return None
+
+
+class Sigmoid(Layer):
+    def __init__(self, input_size, output_size):
+        self.input_size=input_size
+        self.output_size=output_size
+        self.input=None
+        self.output=None
+
+    def forward(self, input):
+        assert isinstance(input,np.ndarray)
+        assert input.shape==(self.input_size,1)
+        self.input=input
+        self.output = 1 / (1 + np.exp(-input))
+        return self.output
+
+    def backward(self, output_gradient):
+        input_gradient=self.output*(1-self.output)*output_gradient
+        return input_gradient
+
