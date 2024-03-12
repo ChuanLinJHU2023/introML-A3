1a6c4d90c585b2c971c970a19a0c3b850e146729
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2024-03-12 22:49:12 +0800

    3.12.22.45

--- DataPredictors.py
+++ DataPredictors.py
@@ -1,7 +1,13 @@
 import numpy as np
+from typing import List
 class Layer:
     def __init__(self, input_size, output_size):
-        raise NotImplementedError
+        self.input_size=input_size
+        self.output_size=output_size
+        self.input=None
+        self.output=None
+        self.params=None
+        self.grads=None
 
     def forward(self, input):
         raise NotImplementedError
@@ -17,65 +23,93 @@ class Layer:
 
 
 class Linear(Layer):
-    def __init__(self, input_size, output_size, W=None, b=None):
-        self.input_size=input_size
-        self.output_size=output_size
-        self.input=None
-        self.output=None
-        self.W=np.random.randn(output_size,input_size) if W is None else W
-        self.b=np.random.randn(output_size,1) if b is None else b
-        self.W_grad=None
-        self.b_grad=None
+    def __init__(self, input_size, output_size, W:np.ndarray = None, b:np.ndarray = None):
+        super().__init__(input_size, output_size)
+        W=np.random.randn(output_size,input_size) if W is None else W
+        b=np.random.randn(output_size,1) if b is None else b
+        W_grad=0
+        b_grad=0
+        self.params=[W,b]
+        self.grads=[W_grad,b_grad]
 
     def forward(self, input):
-        assert isinstance(input,np.ndarray)
         assert input.shape==(self.input_size,1)
         self.input=input
-        self.output=self.W @ input + self.b
+        W, b = self.params
+        self.output= W @ input + b
         return self.output
 
     def backward(self, output_gradient):
-        assert isinstance(output_gradient,np.ndarray)
         assert output_gradient.shape==(1,self.output_size)
-        input_gradient=output_gradient@self.W
-        self.W_grad=output_gradient@self.input
-        self.b_grad=output_gradient
-        assert self.W_grad.shape==self.W.shape
-        assert self.b_grad.shape==self.b.shape
+        W, b = self.params
+        input_gradient=output_gradient @ W
+        W_grad, b_grad = self.grads
+        W_grad += output_gradient.T @ self.input.T
+        b_grad += output_gradient.T
+        assert W_grad.shape==W.shape
+        assert b_grad.shape==b.shape
+        self.grads=[W_grad, b_grad]
         return input_gradient
 
-    def param(self, parameter_name):
+    def param(self, parameter_name=None):
         assert parameter_name
+        W = self.params[0]
+        b = self.params[1]
         if parameter_name=="W":
-            return self.W
+            return W
         if parameter_name=="b":
-            return self.b
+            return b
         return None
 
     def grad(self, parameter_name=None):
         assert parameter_name
+        W_grad=self.grads[0]
+        b_grad=self.grads[1]
         if parameter_name=="W":
-            return self.W_grad
+            return W_grad
         if parameter_name=="b":
-            return self.b_grad
+            return b_grad
         return None
 
 
 class Sigmoid(Layer):
     def __init__(self, input_size, output_size):
-        self.input_size=input_size
-        self.output_size=output_size
-        self.input=None
-        self.output=None
+        super().__init__(input_size, output_size)
+        assert input_size==output_size
 
     def forward(self, input):
-        assert isinstance(input,np.ndarray)
         assert input.shape==(self.input_size,1)
         self.input=input
         self.output = 1 / (1 + np.exp(-input))
         return self.output
 
     def backward(self, output_gradient):
+        assert output_gradient.shape==(1,self.output_size)
         input_gradient=self.output*(1-self.output)*output_gradient
         return input_gradient
+
+
+class Sequential:
+    def __init__(self, layers: List[Layer]):
+        assert isinstance(layers, list)
+        self.layers = layers
+        self.input_size = layers[0].input_size
+        self.output_size = layers[1].input_size
+
+    def forward(self, input):
+        assert input.shape == (self.input_size, 1)
+        intermediate = input
+        for layer in self.layers:
+            intermediate = layer.forward(intermediate)
+        output = intermediate
+        return output
+
+    def backward(self, output_gradient):
+        assert output_gradient.shape == (1, self.output_size)
+        intermediate_gradient = output_gradient
+        for layer in reversed(self.layers):
+            intermediate_gradient = layer.backward(intermediate_gradient)
+        input_gradient = intermediate_gradient
+        return input_gradient
+
 
---
+++ DataTools.py
@@ -0,0 +1,47 @@
+import numpy as np
+from DataPredictors import *
+
+class Loss:
+    def __init__(self, predicted, ground_truth):
+        self.predicted=predicted
+        self.ground_truth=ground_truth
+
+    def value(self):
+        raise NotImplementedError
+
+    def grad(self):
+        raise NotImplementedError
+
+
+class SSE(Loss):
+    def __init__(self, predicted, ground_truth):
+        assert predicted.shape==ground_truth.shape
+        super().__init__(predicted, ground_truth)
+
+    def value(self):
+        value=np.sum(self.predicted - self.ground_truth)
+        return value
+
+    def grad(self):
+        return 2*(self.predicted-self.ground_truth)
+
+
+class Optimizer:
+    def __init__(self,lr):
+        self.learning_rate=lr
+
+    def step(self, seq: Sequential):
+        raise NotImplementedError
+
+
+class GradientDecsent(Optimizer):
+    def step(self, seq: Sequential):
+        for layer in seq.layers:
+            for i in range(len(layer.params)):
+                assert layer.params[i].shape==layer.grads[i].shape
+                shape=layer.params[i].shape
+                layer.params[i]+=layer.grads[i]
+                layer.grads[i]=np.zeros(shape)
+
+
+
