8c2de6ad8de3f66e24bd98bcd53545f47e2d0a6e
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2024-03-13 17:16:28 +0800

    3.13.17.16

--- DataTools.py
+++ DataTools.py
@@ -3,28 +3,23 @@ from DataPredictors import *
 import pandas as pd
 
 class Loss:
-    def __init__(self, predicted, ground_truth):
-        self.predicted=predicted
-        self.ground_truth=ground_truth
+    def __init__(self, input_size, output_size):
+        pass
 
-    def value(self):
+    def value(self, predicted, ground_truth):
         raise NotImplementedError
 
-    def grad(self):
+    def grad(self, predicted, ground_truth):
         raise NotImplementedError
 
 
 class SSE(Loss):
-    def __init__(self, predicted, ground_truth):
-        assert predicted.shape==ground_truth.shape
-        super().__init__(predicted, ground_truth)
-
-    def value(self):
-        value=np.sum(self.predicted - self.ground_truth)
+    def value(self, predicted, ground_truth):
+        value=np.sum(predicted - ground_truth)
         return value
 
-    def grad(self):
-        return 2*(self.predicted-self.ground_truth)
+    def grad(self, predicted, ground_truth):
+        return 2*(predicted-ground_truth)
 
 
 class Optimizer:
@@ -41,7 +36,7 @@ class GradientDecsent(Optimizer):
             for i in range(len(layer.params)):
                 assert layer.params[i].shape==layer.grads[i].shape
                 shape=layer.params[i].shape
-                layer.params[i]+=layer.grads[i]
+                layer.params[i]+=layer.grads[i]*self.learning_rate
                 layer.grads[i]=np.zeros(shape)
 
 
@@ -53,21 +48,45 @@ class Model:
         self.label_feature=label_feature
         self.label_index=None
 
-    def fit(self, df_train: pd.DataFrame, training_mode):
-        assert training_mode=="mini_batch" or training_mode=="incremental" or training_mode=="full"
-        try:
-            arr_train=np.array(df_train)
-        except:
-            raise ValueError
+
+    def fit(self, df_train: pd.DataFrame, training_mode, batch_size, n_epochs):
+        arr_train=np.array(df_train)
         self.label_index=list(df_train.columns).index(self.label_feature)
-        batch_size=self.get_batch_size(df_train, training_mode)
+        datapoint_number=arr_train.shape[0]
+        feature_number=arr_train.shape[1]-1
+        xs=np.delete(arr_train,self.label_index,axis=1)
+        ys=arr_train[:,self.label_index]
+        for epoch_i in range(n_epochs):
+            for batch_i in range(arr_train.shape[0]//batch_size):
+                start=batch_i*batch_size
+                end=min((batch_i+1)*batch_size,datapoint_number)
+                epoch_loss=0
+                for dp_i in range(start,end):
+                    x=xs[dp_i].reshape((feature_number,1))
+                    y=ys[dp_i]
+                    predited=self.seq.forward(x)
+                    ground_truth=y
+                    loss=self.loss.value(predited,ground_truth)
+                    grad=self.loss.grad(predited,ground_truth)
+                    self.seq.backward(grad)
+                    epoch_loss+=loss
+                self.opt.step(self.seq)
+                print("Epoch {}: Loss {}".format(epoch_i,epoch_loss))
 
 
-    def predict(self):
-        pass
-
-    def get_batch_size(self,df_train, training_mode):
-        if
+    def predict(self, df_test: pd.DataFrame):
+        arr_test=np.array(df_test)
+        datapoint_number=arr_test.shape[0]
+        feature_number=arr_test.shape[1]-1
+        xs=np.delete(arr_test,self.label_index,axis=1)
+        ys=np.zeros(datapoint_number)
+        start=0
+        end=datapoint_number
+        for dp_i in range(start, end):
+            x = xs[dp_i].reshape((feature_number, 1))
+            predited = self.seq.forward(x)
+            ys[dp_i] = predited
+        return ys
 
 
 
