ab88e7c14412ed7e01d2ef27fbc06a79d11e170c
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2024-03-14 11:20:04 +0800

    3.14.11.20

--- DataOptimizersLab.py
+++ DataOptimizersLab.py
@@ -9,11 +9,15 @@ def tuple_generate():
     return [random.randint(0,10),random.randint(0,10),random.randint(0,10)]
 example_xs=[ tuple_generate() for i in range(data_size)]
 example_ys=[a+2*b+3*c for (a,b,c) in example_xs]
-
+print("example xs")
+print(example_xs)
+print("example ys")
+print(example_ys)
+print("\n\n")
 
 input_size=3
 output_size=1
-lr=0.001
+lr=0.00001
 n_epochs=20
 W=np.ones((output_size,input_size))
 b=np.zeros((output_size,1))
@@ -28,6 +32,13 @@ for epoch_i in range(n_epochs):
         value=loss.value(y_pred,y)
         grad=loss.grad()
         linear_layer.backward(grad)
+        print("x",x)
+        print("y",y)
+        print("y pred",y_pred)
+        print("value", value)
+        print("W",linear_layer.params[0])
+        print("W_grad",linear_layer.grads[0])
+        print("\n")
     opt.step(linear_layer)
     print(linear_layer.param("W"))
 
