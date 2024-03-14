d175668ce0789b507a67543736ee216e194d7987
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2024-03-14 14:56:55 +0800

    3.14.15。00

--- DataLossersLab.py
+++ DataLossersLab.py
@@ -3,7 +3,14 @@ from DataLossers import *
 # input_size=5
 # loss=SSE(input_size)
 # predicted= [2,3,4,5,6]
-# groundtruth=[1,2,3,4,5]
+# groundtruth=[1,2,2,4,5]
+# print(loss.value(predicted,groundtruth))
+# print(loss.grad())
+
+# input_size=1
+# loss=SSE(input_size)
+# predicted= 2
+# groundtruth= 6
 # print(loss.value(predicted,groundtruth))
 # print(loss.grad())
 
--- DataOptimizersLab.py
+++ DataOptimizersLab.py
@@ -8,6 +8,11 @@ data_size=20
 def tuple_generate():
     return [random.randint(0,10),random.randint(0,10),random.randint(0,10)]
 example_xs=[ tuple_generate() for i in range(data_size)]
+example_xs=\
+    [[3, 1, 10], [7, 1, 2], [8, 2, 4], [4, 7, 9], [9, 4, 0], [6, 6, 6],
+     [4, 0, 6], [2, 4, 0], [1, 4, 7], [0, 8, 6], [6, 3, 10], [8, 10, 5],
+     [6, 2, 5], [10, 3, 9], [7, 2, 1], [1, 2, 4], [10, 4, 6], [10, 5, 7],
+     [5, 9, 4], [1, 4, 0]]
 example_ys=[a+2*b+3*c for (a,b,c) in example_xs]
 print("example xs")
 print(example_xs)
@@ -18,13 +23,19 @@ print("\n\n")
 input_size=3
 output_size=1
 lr=0.00001
-n_epochs=20
+# n_epochs=20
+n_epochs=20000
 W=np.ones((output_size,input_size))
+# W=np.array([1,2,3]).reshape((output_size,input_size)).astype(np.float)
 b=np.zeros((output_size,1))
 linear_layer=Linear(input_size,output_size,W=W,b=b)
 opt=GradientDecsent(lr)
 loss=SSE(output_size)
 for epoch_i in range(n_epochs):
+    print("Epoch {}".format(epoch_i)+"//"*100)
+    print("W", linear_layer.params[0])
+    print("W_grad", linear_layer.grads[0])
+    print("\n")
     for dp_i in range(data_size):
         x=example_xs[dp_i]
         y=example_ys[dp_i]
@@ -32,15 +43,14 @@ for epoch_i in range(n_epochs):
         value=loss.value(y_pred,y)
         grad=loss.grad()
         linear_layer.backward(grad)
-        print("x",x)
-        print("y",y)
-        print("y pred",y_pred)
-        print("value", value)
-        print("W",linear_layer.params[0])
-        print("W_grad",linear_layer.grads[0])
-        print("\n")
+        # print("x",x)
+        # print("y",y)
+        # print("y pred",y_pred)
+        # print("value", value)
+        # print("W",linear_layer.params[0])
+        # print("W_grad",linear_layer.grads[0])
+        # print("\n")
     opt.step(linear_layer)
-    print(linear_layer.param("W"))
 
 
 
